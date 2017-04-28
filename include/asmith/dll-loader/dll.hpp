32b1644edb1faf14ec834f7a5cc7908dc2a8881a
Adam Smith <adam.george.smith@btinternet.com>
2017-04-28 11:27:15 +0100

    Renamed dll_loader.hpp

--- include/asmith/dll-loader/dll.hpp
+++ include/asmith/dll-loader/dll.hpp
@@ -11,8 +11,8 @@
 //	See the License for the specific language governing permissions and
 //	limitations under the License.
 
-#ifndef ASMITH_DLL_DLL_LOADER_HPP
-#define ASMITH_DLL_DLL_LOADER_HPP
+#ifndef ASMITH_DLL_DLL_HPP
+#define ASMITH_DLL_DLL_HPP
 
 #include <string>
 #include <memory>
