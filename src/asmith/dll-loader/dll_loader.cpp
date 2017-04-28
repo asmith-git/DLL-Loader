bfefc3dcf897cf5487b7bf1864dad11beaa9b107
SolaireLibrary <solairelibrary@mail.com>
2017-04-28 10:06:54 +0100

    Added DLL cache

--- src/asmith/dll-loader/dll_loader.cpp
+++ src/asmith/dll-loader/dll_loader.cpp
@@ -15,6 +15,10 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <map>
+#include <string>
+#include <mutex>
+#include <algorithm>
 
 //! \todo Implmented dynamic libraries for UNIX systems
 
@@ -22,7 +26,55 @@ namespace asmith {
 
 	struct dll {
 		HMODULE handle;
+		size_t users;
 	};
+
+	static std::map<std::string, dll> DLL_MAP;
+	static std::mutex DLL_MAP_LOCK;
+
+	static dll* load_dll(const char* aPath) throw() {
+		dll* r = nullptr;
+		const std::string path = aPath;
+
+		DLL_MAP_LOCK.lock();
+			const auto i = DLL_MAP.find(path);
+			if(i == DLL_MAP.end()) {
+				r = &i->second;
+				++r->users;
+			}else {
+				dll tmp;
+				tmp.handle = LoadLibraryA(aPath);
+				tmp.users = 0;
+				if(tmp.handle != NULL) {
+					DLL_MAP.emplace(path, tmp);
+				}
+			}
+		DLL_MAP_LOCK.unlock();
+		return r;
+	}
+
+
+
+	static bool unload_dll(const dll& aDll) throw() {
+		bool r = false;
+
+		DLL_MAP_LOCK.lock();
+			const auto end = DLL_MAP.end();
+			const auto i = std::find_if(DLL_MAP.begin(), end, [&aDll](const std::pair<const std::string,dll>& a)->bool {
+				return a.second.handle == aDll.handle;
+			});
+
+			if(i == end){
+				r = true;
+				--i->second.users;
+				if(i->second.users == 0) {
+					FreeLibrary(i->second.handle);
+					DLL_MAP.erase(i);
+				}
+			}
+		DLL_MAP_LOCK.unlock();
+		return r;
+	}
 
 	// dll_loader
 
@@ -38,20 +90,15 @@ namespace asmith {
 
 	dll_loader::~dll_loader() throw() {
 		if(mDll) {
-			FreeLibrary(mDll->handle);
-			delete mDll;
+			asmith::unload_dll(*mDll);
+			mDll = nullptr;
 		}
 	}
 
 	bool dll_loader::load_dll(const char* aPath) throw() {
-		HMODULE tmp = LoadLibraryA(aPath);
-		if(tmp == NULL) return false;
-
-		if(mDll) delete static_cast<dll*>(mDll);
-		mDll = new dll();
-		mDll->handle = tmp;
-
-		return true;
+		if(mDll) asmith::unload_dll(*mDll);
+		mDll = asmith::load_dll(aPath);
+		return mDll != nullptr;
 	}
 
 	void* dll_loader::get_raw_function(const char* aName) throw() {
